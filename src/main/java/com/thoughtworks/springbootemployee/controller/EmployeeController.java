274e66e80c41a5363575fe9bad6a1e5cae329b09
Eason <chenzscd@163.com>
2020-07-28 21:21:29 +0800

    [simple Restful API][Eason] feature: implement APIs in EmployeeController

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
@@ -1,11 +1,43 @@
 package com.thoughtworks.springbootemployee.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.thoughtworks.springbootemployee.model.Employee;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/employees")
 public class EmployeeController {
+
+    @GetMapping
+    public List<Employee> getAllEmployee() {
+        List<Employee> employees = new ArrayList<>();
+        employees.add(new Employee(0, "Xiaoming", 20, "Male"));
+        employees.add(new Employee(1, "Xiaohong", 19, "Female"));
+        employees.add(new Employee(2, "Xiaozhi", 15, "Male"));
+        employees.add(new Employee(3, "Xiaogang", 16, "Male"));
+        employees.add(new Employee(4, "Xiaoxia", 15, "Female"));
+        return employees;
+    }
+
+    @GetMapping("/{id}")
+    public Employee getEmployeeById(@PathVariable int id) {
+        return new Employee(id, "Xiaoming", 20, "Male");
+    }
+
+    @PutMapping("/{id}")
+    public Employee updateEmployee(@PathVariable int id, @RequestBody Employee employee) {
+        return new Employee(id, employee.getName(), employee.getAge(), employee.getGender());
+    }
+
+    @PostMapping
+    public Employee insertEmployee(@RequestBody Employee employee) {
+        return employee;
+    }
+
+    @DeleteMapping
+    public Employee deleteEmployee(@PathVariable int id) {
+        return new Employee(id, "Xiaoxia", 15, "Female");
+    }
 }
